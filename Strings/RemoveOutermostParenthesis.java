329d04a2f4ee0132f22190ba27db207baa00fdf5
Akshaya S <akshayareddy4947@gmail.com>
2026-04-15 00:26:55 +0530

    Add files via upload

--- Strings/RemoveOutermostParenthesis.java
+++ Strings/RemoveOutermostParenthesis.java
@@ -1,4 +1,26 @@
-package arraysEasy.Strings;
 
 public class RemoveOutermostParenthesis {
+    public static void main(String[] args) {
+        String s = "(()())(())";
+        System.out.println(remove(s));
+    }
+
+    static String remove(String s){
+        StringBuilder s1 = new StringBuilder();
+        int count = 0;
+        for(char ch : s.toCharArray()){
+            if(ch == '('){
+                if(count>0){
+                    s1.append(ch);
+                }
+                count++;
+            }else if(ch == ')'){
+                count--;
+                if(count>0){
+                    s1.append(ch);
+                }
+            }
+        }
+        return s1.toString();
+    }
 }
